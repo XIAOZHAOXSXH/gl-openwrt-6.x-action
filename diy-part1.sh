61ec78410e6863f70c7f24020e155fc17eab5bc8
XIAOZHAOXSXH <112842045+XIAOZHAOXSXH@users.noreply.github.com>
2024-12-18 11:34:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# diy-part1.sh
 #
 
-# Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add feed sources
+# 添加 Feed 源
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
 
-# Clone luci-theme-design into package directory
+# 克隆 luci-theme-design 主题到 package 目录
 git clone --depth 1 https://github.com/SAENE/luci-theme-design.git package/luci-theme-design

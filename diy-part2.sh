c93a7c303787a2f26caa73b93fa33ce29a227d2c
XIAOZHAOXSXH <112842045+XIAOZHAOXSXH@users.noreply.github.com>
2024-12-18 11:33:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,24 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# Modify default theme to luci-theme-design
+# Ensure luci-theme-design is selected in the configuration
+# This assumes that luci-theme-design is correctly cloned into package/luci-theme-design
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# Use uci to set the default theme
+# Note: This requires that the theme is already compiled and available in the build system
+# Alternatively, you can directly modify the .config file to select the theme
+
+# Uncomment the following lines if you want to modify the .config directly
+# sed -i 's/^#\(CONFIG_PACKAGE_luci-theme-design=y\)/\1/' .config
+# sed -i 's/^CONFIG_THEME_luci-theme-bootstrap/CONFIG_THEME_luci-theme-design/' .config
+
+# Alternatively, set the theme using uci
+# This method might not work directly during the build process, so it's safer to modify .config
+
+# Example of modifying .config to set the default theme
+# Add luci-theme-design to the selected themes
+echo 'CONFIG_PACKAGE_luci-theme-design=y' >> .config
+echo 'CONFIG_DEFAULT_THEME="luci-theme-design"' >> .config
